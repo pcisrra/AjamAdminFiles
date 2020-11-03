c4d2a662d75b319ccc095a97e0416b62958065e3
pcisrra <deshumanizer97@gamil.com>
2020-11-02 21:43:45 -0400

    Menu modal change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -104,14 +104,14 @@
 				</div>
 				<div class="modal fade" id="modalGlosario" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 					<div class="modal-dialog" role="document">
-						<div class="modal-content">
+						<div class="modal-content glosario-image responsive">
 							<div class="modal-header">
 								<h4 class="modal-title" id="myModalLabel">Glosario</h4>
 								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 								<span aria-hidden="true">×</span>
 								</button>
 							</div>
-							<div class="modal-footer">
+							<div class="modal-footer glosario-image responsive">
 								<img alt="logo" src="{{ asset('images/glosario.jpeg') }}" class="image-sizer responsive">
 							</div>
 						</div>
