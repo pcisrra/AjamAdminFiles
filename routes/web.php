bac1a7c982fab85f80da67b04f14cc0d974f6a30
pcisrra <deshumanizer97@gamil.com>
2020-11-02 21:42:45 -0400

    Routes addition ChangeState

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,18 @@ Route::resource('regional','RegionalController');
 Route::resource('gaceta','GacetaController');
 Route::resource('unidad_tecnica','UnidadTecnicaController');
 
+Route::get('/juridica/ChangeState/{id}/{estado}','JuridicaController@ChangeState')->name('juridica.ChangeState');
+Route::get('/auditoria/ChangeState/{id}/{estado}','AuditoriaController@ChangeState')->name('auditoria.ChangeState');
+Route::get('/empaste/ChangeState/{id}/{estado}','EmpastesController@ChangeState')->name('empaste.ChangeState');
+Route::get('/asesor/ChangeState/{id}/{estado}','AsesorController@ChangeState')->name('asesor.ChangeState');
+Route::get('/ilegal/ChangeState/{id}/{estado}','IlegalController@ChangeState')->name('ilegal.ChangeState');
+Route::get('/planificacion/ChangeState/{id}/{estado}','PlanificacionController@ChangeState')->name('planificacion.ChangeState');
 Route::get('/fiscalizacion/ChangeState/{id}/{estado}','FiscalizacionController@ChangeState')->name('fiscalizacion.ChangeState');
+Route::get('/recursoHumano/ChangeState/{id}/{estado}','RecursoHumanoController@ChangeState')->name('recursoHumano.ChangeState');
+Route::get('/fondo/ChangeState/{id}/{estado}','FondoController@ChangeState')->name('fondo.ChangeState');
+Route::get('/regional/ChangeState/{id}/{estado}','RegionalController@ChangeState')->name('regional.ChangeState');
+Route::get('/gaceta/ChangeState/{id}/{estado}','GacetaController@ChangeState')->name('gaceta.ChangeState');
+Route::get('/unidad_tecnica/ChangeState/{id}/{estado}','UnidadTecnicaController@ChangeState')->name('unidad_tecnica.ChangeState');
 
 Route::get('/home', function () {
     if (session('status')) {
